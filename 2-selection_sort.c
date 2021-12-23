c1b1d4f3f48e3a3f83b40d0588076247a2863088
alixvox <alixvox@gmail.com>
2021-12-22 19:47:29 -0800

    Fixing selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,7 +10,7 @@ void selection_sort(int *array, size_t size)
 	int i, j, lowest_idx, swap;
 	int s = (int)size;
 
-	for (i = 0; i < s; i++)
+	for (i = 0; i < s - 1; i++)
 	{
 		lowest_idx = i;
 		for (j = i + 1; j < s; j++)
@@ -18,9 +18,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[lowest_idx])
 				lowest_idx = j;
 		}
-		swap = array[i];
-		array[i] = array[lowest_idx];
-		array[lowest_idx] = swap;
-		print_array(array, size);
+		if (i != lowest_idx)
+		{
+			swap = array[i];
+			array[i] = array[lowest_idx];
+			array[lowest_idx] = swap;
+			print_array(array, size);
+		}
 	}
 }
