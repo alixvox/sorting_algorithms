6a1f153590afa46a3ffa6a24f19bf3d007cc8cc0
alixvox <alixvox@gmail.com>
2022-05-06 00:02:20 -0500

    hi

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,18 +14,22 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort_rec(int *array, int left, int right, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort_2(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void mergeSort(int *array, int start, int end, int *temp_array);
+void merge(int *array, int start, int mid, int end, int *temp_array);
 
-#endif /** SORT_H **/
+#endif
