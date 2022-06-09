cb79d6adb402ef433fd21e2a067931ef8d041f80
alixvox <alixvox@gmail.com>
2022-06-09 00:16:23 -0500

    Final changes?

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -15,44 +15,35 @@ void radix_sort(int *array, size_t size)
   int max = get_max_int(array, size);
   int *output;
   int *count;
-  int maxPos;
-  int i;
+  int maxPos, i, pos;
 
-  // For loop of the radix sort (Technically a countingSort)
-  for (int pos = 1; max / pos > 0; pos *= 10)
+  for (pos = 1; max / pos > 0; pos *= 10)
   {
     output = malloc((size + 1) * sizeof(int));
-    // Getting max number of digits
     maxPos = (array[0] / pos) % 10;
 
-    // Find the highest digit of the position
     for (i = 1; i < (int)size; i++)
       if (((array[i] / pos) % 10) > maxPos)
         maxPos = array[i];
 
-    // Establishing a counter array to count number of ints in each position
     count = malloc((max + 1) * sizeof(int));
     for (i = 0; i < maxPos; ++i)
       count[i] = 0;
     for (i = 1; i < (int)size; i++)
       count[(array[i] / pos) % 10]++;
 
-    // Cumulative count of all ints in each position
     for (i = 1; i < 10; i++)
       count[i] += count[i - 1];
 
-    // Place ints in sorted order depending on position
     for (i = (int)size; i >= 0; i--)
     {
       output[count[(array[i] / pos) % 10] - 1] = array[i];
       count[(array[i] / pos) % 10]--;
     }
 
-    // Assign to original array
     for (i = 0; i < (int)size; i++)
       array[i] = output[i];
 
-    // Print_array
     print_array(array, size);
 
     free(output);
@@ -70,7 +61,8 @@ void radix_sort(int *array, size_t size)
 int get_max_int(int *array, size_t size)
 {
   int max = 0;
-  for (int i = 0; i < (int)size; i++)
+  int i;
+  for (i = 0; i < (int)size; i++)
     if (array[i] > max)
       max = array[i];
   return max;
